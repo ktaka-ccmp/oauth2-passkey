d29f15d843924e9f1596624684cba58d29ff2eec
Kimitoshi Takahashi <ktaka@ccmp.jp>
2025-02-15 10:26:19 +0900

    Imported axum router codes from liboauth2, libsession and libpasskey.

---
+++ demo-integration/src/oauth2/handlers.rs
@@ -0,0 +1,176 @@
+use askama::Template;
+use axum::{
+    extract::{Form, Query},
+    http::{HeaderMap, StatusCode},
+    response::{Html, Redirect},
+    routing::get,
+    Router,
+};
+use axum_extra::{headers, TypedHeader};
+use chrono::{Duration, Utc};
+
+// Helper trait for converting errors to a standard response error format
+trait IntoResponseError<T> {
+    fn into_response_error(self) -> Result<T, (StatusCode, String)>;
+}
+
+impl<T, E: std::fmt::Display> IntoResponseError<T> for Result<T, E> {
+    fn into_response_error(self) -> Result<T, (StatusCode, String)> {
+        self.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+    }
+}
+
+use liboauth2::{
+    csrf_checks, get_idinfo_userinfo, header_set_cookie, prepare_oauth2_auth_request,
+    validate_origin, AuthResponse, OAUTH2_AUTH_URL, OAUTH2_CSRF_COOKIE_NAME, OAUTH2_ROUTE_PREFIX,
+};
+
+use libsession::{
+    create_new_session, delete_session_from_store, prepare_logout_response, SessionInfo,
+    SESSION_COOKIE_MAX_AGE, SESSION_COOKIE_NAME,
+};
+
+use libuserdb::{upsert_user, User as DbUser};
+
+pub fn router() -> Router {
+    Router::new()
+        .route("/google", get(google_auth))
+        .route("/oauth2.js", get(oauth2_js))
+        .route("/authorized", get(get_authorized).post(post_authorized))
+        .route("/popup_close", get(popup_close))
+        .route("/logout", get(logout))
+}
+
+#[derive(Template)]
+#[template(path = "popup_close.j2")]
+struct PopupCloseTemplate;
+
+#[derive(Template)]
+#[template(path = "oauth2_js.j2")]
+struct OAuth2JsTemplate<'a> {
+    auth_route_prefix: &'a str,
+}
+
+pub(crate) async fn popup_close() -> Result<Html<String>, (StatusCode, String)> {
+    let template = PopupCloseTemplate;
+    let html = Html(template.render().into_response_error()?);
+    Ok(html)
+}
+
+pub(crate) async fn oauth2_js() -> Result<(HeaderMap, String), (StatusCode, String)> {
+    let template = OAuth2JsTemplate {
+        auth_route_prefix: OAUTH2_ROUTE_PREFIX.as_str(),
+    };
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        http::header::CONTENT_TYPE,
+        "application/javascript".parse().unwrap(),
+    );
+
+    Ok((headers, template.render().into_response_error()?))
+}
+
+pub(crate) async fn google_auth(
+    headers: HeaderMap,
+) -> Result<(HeaderMap, Redirect), (StatusCode, String)> {
+    let (auth_url, headers) = prepare_oauth2_auth_request(headers)
+        .await
+        .into_response_error()?;
+
+    Ok((headers, Redirect::to(&auth_url)))
+}
+
+pub async fn logout(
+    TypedHeader(cookies): TypedHeader<headers::Cookie>,
+) -> Result<(HeaderMap, Redirect), (StatusCode, String)> {
+    let headers = prepare_logout_response(cookies)
+        .await
+        .into_response_error()?;
+    Ok((headers, Redirect::to("/")))
+}
+
+pub async fn post_authorized(
+    TypedHeader(cookies): TypedHeader<headers::Cookie>,
+    headers: HeaderMap,
+    Form(form): Form<AuthResponse>,
+) -> Result<(HeaderMap, Redirect), (StatusCode, String)> {
+    #[cfg(debug_assertions)]
+    println!(
+        "Cookies: {:#?}",
+        cookies.get(OAUTH2_CSRF_COOKIE_NAME.as_str())
+    );
+
+    validate_origin(&headers, OAUTH2_AUTH_URL.as_str())
+        .await
+        .into_response_error()?;
+
+    if form.state.is_empty() {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            "Missing state parameter".to_string(),
+        ));
+    }
+
+    authorized(&form).await
+}
+
+pub async fn get_authorized(
+    Query(query): Query<AuthResponse>,
+    TypedHeader(cookies): TypedHeader<headers::Cookie>,
+    headers: HeaderMap,
+) -> Result<(HeaderMap, Redirect), (StatusCode, String)> {
+    validate_origin(&headers, OAUTH2_AUTH_URL.as_str())
+        .await
+        .into_response_error()?;
+    csrf_checks(cookies.clone(), &query, headers)
+        .await
+        .into_response_error()?;
+
+    delete_session_from_store(cookies, SESSION_COOKIE_NAME.to_string())
+        .await
+        .into_response_error()?;
+
+    authorized(&query).await
+}
+
+async fn authorized(
+    auth_response: &AuthResponse,
+) -> Result<(HeaderMap, Redirect), (StatusCode, String)> {
+    let (idinfo, _userinfo) = get_idinfo_userinfo(auth_response)
+        .await
+        .into_response_error()?;
+
+    // Convert GoogleUserInfo to DbUser and store it
+    // let user_userinfo = DbUser::from(userinfo);
+
+    // Convert GoogleIdInfo to DbUser and store it
+    let user_idinfo = DbUser::from(idinfo.clone());
+
+    // Store the user in the database
+    let user = upsert_user(user_idinfo).await.into_response_error()?;
+
+    // Create minimal session info
+    let session_info = SessionInfo {
+        user_id: user.id,
+        expires_at: Utc::now() + Duration::seconds(*SESSION_COOKIE_MAX_AGE as i64),
+    };
+
+    let mut headers = create_new_session(session_info)
+        .await
+        .into_response_error()?;
+
+    let _ = header_set_cookie(
+        &mut headers,
+        OAUTH2_CSRF_COOKIE_NAME.to_string(),
+        "value".to_string(),
+        Utc::now() - Duration::seconds(86400),
+        -86400,
+    )
+    .into_response_error()?;
+
+    Ok((
+        headers,
+        Redirect::to(&format!("{}/popup_close", OAUTH2_ROUTE_PREFIX.as_str())),
+    ))
+}
---
+++ demo-integration/src/session/extractor.rs
@@ -0,0 +1,80 @@
+use axum::{
+    extract::{FromRequestParts, OptionalFromRequestParts},
+    response::{IntoResponse, Redirect, Response},
+    RequestPartsExt,
+};
+use axum_extra::{headers, TypedHeader};
+use http::request::Parts;
+
+use libsession::User;
+use libsession::{SESSION_COOKIE_NAME, get_user_from_session, SessionError};
+
+pub struct AuthRedirect;
+
+impl IntoResponse for AuthRedirect {
+    fn into_response(self) -> Response {
+        println!("AuthRedirect called.");
+        Redirect::temporary("/").into_response()
+    }
+}
+
+impl From<SessionError> for AuthRedirect {
+    fn from(_: SessionError) -> Self {
+        AuthRedirect
+    }
+}
+
+/// A local wrapper around libsession::User to allow implementing foreign traits
+#[derive(Clone)]
+pub struct AuthUser(User);
+
+impl std::ops::Deref for AuthUser {
+    type Target = User;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<User> for AuthUser {
+    fn from(user: User) -> Self {
+        Self(user)
+    }
+}
+
+impl<B> FromRequestParts<B> for AuthUser
+where
+    B: Send + Sync,
+{
+    type Rejection = AuthRedirect;
+
+    async fn from_request_parts(parts: &mut Parts, _: &B) -> Result<Self, Self::Rejection> {
+        let cookies: TypedHeader<headers::Cookie> = parts
+            .extract()
+            .await
+            .map_err(|_| AuthRedirect)?;
+
+        // Get session from cookie
+        let session_cookie = cookies
+            .get(SESSION_COOKIE_NAME.as_str())
+            .ok_or(AuthRedirect)?
+            .to_string();
+
+        // Convert libuserdb::User to libsession::User to AuthUser
+        let db_user: libuserdb::User = get_user_from_session(&session_cookie).await.map_err(AuthRedirect::from)?;
+        let user: User = User::from(db_user);
+        Ok(AuthUser::from(user))
+    }
+}
+
+impl<B> OptionalFromRequestParts<B> for AuthUser
+where
+    B: Send + Sync,
+{
+    type Rejection = AuthRedirect;
+
+    async fn from_request_parts(parts: &mut Parts, state: &B) -> Result<Option<Self>, Self::Rejection> {
+        let result: Result<Self, Self::Rejection> = <AuthUser as FromRequestParts<B>>::from_request_parts(parts, state).await;
+        Ok(result.ok())
+    }
+}
