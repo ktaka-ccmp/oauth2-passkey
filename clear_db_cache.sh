c5df6a5bbae887573617a95fba388b27042ad162
Kimitoshi Takahashi <ktaka@ccmp.jp>
2025-03-12 02:14:18 +0900

    Update: clear_db_cache.sh,monitor_db.sh

--- clear_db_cache.sh
+++ clear_db_cache.sh
@@ -5,5 +5,5 @@ DB_STRING="sqlite3 /tmp/sqlite.db"
 
 echo "drop table users"| $DB_STRING
 echo "drop table passkey_credentials"| $DB_STRING
+echo "drop table oauth2_accounts"| $DB_STRING
 redis-cli flushall
-
--- monitor_db.sh
+++ monitor_db.sh
@@ -9,7 +9,7 @@ SQLITE_HEADERS=".headers on"
 SQLITE_MODE=".mode column"
 
 # Define queries for each table
-QUERY_USERS="select id,created_at from users;"
+QUERY_USERS="select id,name,created_at from users;"
 QUERY_PASSKEY_CREDENTIALS="select user_id,credential_id,user_handle,user_name,user_display_name,created_at from passkey_credentials;"
 QUERY_OAUTH2_ACCOUNTS="select user_id,id,provider_user_id,email,created_at from oauth2_accounts;"
 
