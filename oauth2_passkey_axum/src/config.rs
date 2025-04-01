75d9bb5c6dc5526307cbdc767a61735d2c944915
Kimitoshi Takahashi <ktaka@ccmp.jp>
2025-04-02 08:53:10 +0900

    Minor fix

--- oauth2_passkey_axum/src/config.rs
+++ oauth2_passkey_axum/src/config.rs
@@ -9,7 +9,6 @@ use oauth2_passkey::O2P_ROUTE_PREFIX;
 /// This is the main prefix under which all authentication endpoints will be mounted.
 /// Default: "/o2p"
 pub static O2P_REDIRECT_ANON: LazyLock<String> = LazyLock::new(|| {
-    // std::env::var("O2P_REDIRECT_ANON").unwrap_or_else(|_| "/o2p/user/login".to_string())
     std::env::var("O2P_REDIRECT_ANON").unwrap_or_else(|_| format!("{}/user/login", *O2P_ROUTE_PREFIX))
 });
 
